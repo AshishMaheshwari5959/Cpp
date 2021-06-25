ce63364f5957fbf9527f838d911671819b9da435
Ashish Maheshwari <53733415+AshishMaheshwari5959@users.noreply.github.com>
2021-06-25 21:57:59 +0530

    Create Stack Infix to Postfix.cpp

---
+++ Stack Infix to Postfix.cpp
@@ -0,0 +1,95 @@
+#include <stdlib.h>
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int MAXSIZE = 8;
+int stack[8];
+int top=-1;
+
+bool isempty(){
+    if(top==-1)
+        return 1;
+    else
+        return 0;
+}
+
+bool isfull(){
+    if(top==MAXSIZE)
+        return 1;
+    else
+        return 0;
+}
+
+int peek(int index){
+    if(index>-1 && index<MAXSIZE){
+        return stack[index];
+    } else {
+        return -1;
+    }
+}
+
+int pop(){
+    int data;
+    if(!isempty()){
+        data = stack[top];
+        top--;
+        return data;
+    } else {
+        return -1;
+    }
+}
+
+void push(int data){
+    if(!isfull()){
+        top++;
+        stack[top]=data;
+    } else {
+        cout<<"Stack is full\n";
+    }
+}
+
+int main()
+{
+    string expression;
+    cout<<"Enter the expression : ";
+    cin>>expression;
+    for(int i=0; i<expression.length(); i++){
+        char c = expression[i];
+        if(c=='+' || c=='-' || c=='*' || c=='/' || c=='^' || c=='(' || c==')'){
+            if(isempty()){
+                push(c);
+            } else{
+                if(c=='('){
+                    push(c);
+                } else if(c==')'){
+                    while(stack[top]!='('){
+                        char data;
+                        data = pop();
+                        cout<<data;
+                    }
+                    char data = pop();
+                } else if(c=='+' || c=='-'){
+                    if(stack[top]=='('){
+                        push(c);
+                    } else{
+                        while(!isempty()){
+                            char data;
+                            data = pop();
+                            cout<<data;
+                        }
+                        push(c);
+                    }
+                }
+            }
+        } else {
+            cout<<c;
+        }
+    }
+    while(!isempty()){
+        char data;
+        data=pop();
+        cout<<data;
+    }
+    return 0;
+}
